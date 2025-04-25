2170d83a3e9a359c0f1ce69cc3378b8064ee6301
Junatan Marc Cadayona <m4rc24@yahoo.com>
2025-04-25 18:26:34 +0800

    fix hero section margin. Too high for lg screen

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,7 +1,7 @@
  <!-- Header -->
  <header  class="header py-8 mb:py-28 text-center md:pt-36 lg:text-left xl:pt-44 xl:pb-20">
     <div class="container px-4 sm:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
-        <div class="mb-4 md:mb-16 lg:mt-32 xl:mt-30 xl:mr-12">
+        <div class="mb-4 md:mb-16 lg:mt-20 xl:mt-30 xl:mr-12">
             <h1 class="h1-large mb-5">Be debt-free and take back control of your life.</h1>
             <p class="p-large mb-4">We at Debt Relief are prepared to assist. Regardless of your debt level, financial freedom is possible!</p>
             <ul class="list-none mb-8">
