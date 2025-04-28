bd6041581a8674532070f58dbade6bf3b194f5bd
Junatan  Marc Cadayona <ljohnolayvar32@gmail.com>
2025-04-28 23:46:35 +0800

    added login and logout

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
