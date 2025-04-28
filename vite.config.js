e590e9bccfb8a4d57d01cebc4f5cb3729a455940
Junatan  Marc Cadayona <ljohnolayvar32@gmail.com>
2025-04-29 00:48:33 +0800

    update vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,25 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/*.css", "resources/js/*.js"],
+            input: [
+                "resources/css/admin.css",
+                "resources/css/app.css",
+                "resources/css/datatable.buttons.css",
+                "resources/css/datatables.css",
+                "resources/css/fontawesome-all.css",
+                "resources/css/magnific-popup.css",
+                "resources/css/styles.css",
+                "resources/css/swiper.css",
+                "resources/js/admin.js",
+                "resources/js/app.js",
+                "resources/js/bootstrap.js",
+                "resources/js/datatables.min.js",
+                "resources/js/jquery.easing.min.js",
+                "resources/js/jquery.magnific-popup.js",
+                "resources/js/jquery.min.js",
+                "resources/js/scripts.js",
+                "resources/js/swiper.min.js",
+            ],
             refresh: true,
         }),
         tailwindcss(),
