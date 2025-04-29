4a1aa0c7c769806a161daf6998cec25ce275b6d2
Junatan Marc Cadayona <m4rc24@yahoo.com>
2025-04-29 10:07:32 +0800

    update vite

--- resources/views/layout/landing-page.blade.php
+++ resources/views/layout/landing-page.blade.php
@@ -27,11 +27,13 @@
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet" />
         <link href="https://themewagon.github.io/pavo/css/fontawesome-all.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <script src="https://themewagon.github.io/pavo/js/jquery.magnific-popup.js"></script>
+
         @vite(['resources/css/app.css', 'resources/css/styles.css', 
-        'resources/css/magnific-popup.css', 'resources/css/swiper.css', 
-        'resources/css/magnific-popup.css', 'resources/js/jquery.min.js',
+        'resources/css/magnific-popup.css', 'resources/css/swiper.css',
         'resources/js/jquery.easing.min.js',  'resources/js/app.js',
-        'resources/js/jquery.magnific-popup.js', 'resources/js/scripts.js'])
+         'resources/js/scripts.js'])
 
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
