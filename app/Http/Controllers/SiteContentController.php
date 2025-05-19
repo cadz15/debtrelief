bccb5c1f95c84e9ada18b66c4d3861b0ba24b27c
Junatan Marc Cadayona <m4rc24@yahoo.com>
2025-05-19 11:56:09 +0800

    update fix

--- app/Http/Controllers/SiteContentController.php
+++ app/Http/Controllers/SiteContentController.php
@@ -6,6 +6,7 @@ use App\Models\Faq;
 use App\Models\SiteContent;
 use App\Models\Testimonial;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Yajra\DataTables\DataTables;
 
 class SiteContentController extends Controller
@@ -70,6 +71,12 @@ class SiteContentController extends Controller
         if ($request->hasFile('video')) {
             $video = $request->file('video');
             $fileName = 'video.' . $video->getClientOriginalExtension();
+
+            $path = storage_path('app/public/uploads/' . $fileName);
+            if(Storage::exists($path)) {
+                unlink($path);
+            }
+
             $video->storeAs('uploads', $fileName, 'public');
         } else {
             $filename = null;
@@ -91,7 +98,6 @@ class SiteContentController extends Controller
             ]);
         }
 
-        dd(route('getFile', $fileName));
 
         return redirect()->back()->with('success', 'Home video updated successfully.');
     }
