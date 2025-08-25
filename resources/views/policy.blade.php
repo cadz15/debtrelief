34600fb6184609078635b7e5ac80295ee1fc26e6
Junatan  Marc Cadayona <ljohnolayvar32@gmail.com>
2025-08-25 23:29:08 +0800

    update policy

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -9,7 +9,7 @@
         <article class="prose prose-lg prose-gray max-w-none space-y-8">
             
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Introduction</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Introduction</h2>
                 <p>FCA National Service.org DEBT RELIEF (“FCA National Service.org DEBT RELIEF”, “we” or “us”) is sensitive to concerns that personal information collected from its customers (“you” or “your”) be handled respectfully and appropriately.</p>
                 <p>This Privacy Policy (the “Policy”) discloses the privacy practices for the Site and applies solely to information collected by the website <a href="https://fcanationalservice.org/" class="text-blue-600">https://fcanationalservice.org/</a> including any related domains and sub-domains (the “Site”).</p>
                 <p>This Policy is intended to notify you of the following:</p>
@@ -22,75 +22,75 @@
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800 mb-6">What Information Is Collected?</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">What Information Is Collected?</h2>
                 
-                <h3 class="text-xl font-semibold text-gray-800">Internet Traffic Data</h3>
+                <h3 class="text-xl font-semibold text-gray-800  mb-4 mt-4">Internet Traffic Data</h3>
                 <p>Internet traffic data includes but is not limited to Internet service provider information, collection of IP addresses, entry and exit pages, various operating systems, and other such information. We collect no personally identifying information through this process, and it is used to help analyze user traffic with respect to various services available through the Site to allow for a more customized and enjoyable user experience.</p>
                 
-                <h3 class="text-xl font-semibold text-gray-800">Cookies</h3>
+                <h3 class="text-xl font-semibold text-gray-800 mb-4 mt-4">Cookies</h3>
                 <p>We use “cookies” on this Site. A cookie is a piece of data stored on a site visitor’s hard drive to help us improve your access to our site and identify repeat visitors. For instance, when we use a cookie to identify you, you do not have to log in a password more than once, saving time and improving efficiency.</p>
                 <p>Cookies can also enable us to track and target the interests of our users to enhance the experience on our site. Usage of a cookie is in no way linked to any personally identifiable information on our Site.</p>
                 
-                <h3 class="text-xl font-semibold text-gray-800">Personal Information</h3>
+                <h3 class="text-xl font-semibold text-gray-800 mb-4 mt-4">Personal Information</h3>
                 <p>The Site collects some information from you (“Identifier Information”) to allow you to use and access services, products, and materials. Identifier Information is only collected when you personally provide such information to the Site via e-mail, by phone, or otherwise through the Site’s website when making a purchase or requesting service.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Information Collection, Use, and Sharing</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Information Collection, Use, and Sharing</h2>
                 <p>No mobile information will be shared with third parties/affiliates for marketing/promotional purposes. Information sharing to subcontractors in support services, such as customer service, is permitted. We will use your information to respond to you, and will not share your information with any third party outside of our organization, other than as necessary to fulfill your request.</p>
                 <p>Unless you ask us not to, we may contact you via email in the future to tell you about specials, new products or services, or changes to this privacy policy.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Your Access to and Control Over Information</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Your Access to and Control Over Information</h2>
                 <p>You may opt out of any future contacts from us or request that we discontinue sending email and other communications at any time by contacting us at the information included within this Policy.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Security</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Security</h2>
                 <p>We take precautions to protect your information. When you submit sensitive information via the Site, your information is protected both online and offline.</p>
                 <p>Wherever we collect sensitive information, that information is encrypted and transmitted to us via secure servers. Only employees who need the information to perform specific jobs (for example, billing or customer service) are granted access to personally identifiable information. The computers/servers in which we store personally identifiable information are kept in a secure environment.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Registration</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Registration</h2>
                 <p>In order to use this site, a user may complete the registration form. During registration, a user is required to give certain information (such as name and email address). This information is used to contact you about the products/services on our site in which you have expressed interest. At your option, you may also provide demographic information (such as gender or age) about yourself, but it is not required.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Orders</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Orders</h2>
                 <p>We may request information from you on our order form. To buy from us, you must provide contact information (such as your name and shipping address) and financial information (like credit card number, expiration date). This information is used for billing purposes and to fill your orders. If we have trouble processing an order, we will use this information to contact you.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Third Parties</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Third Parties</h2>
                 <p>Some of our business partners may use cookies or other methods of tracking and targeting user behavior on our site (advertisers, for example). However, we have no access to or control over these cookies, and this Privacy Policy does not extend to such third parties. We are not responsible for the content or privacy practices of these third parties.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Surveys & Contests</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Surveys & Contests</h2>
                 <p>From time to time, our site may request information via surveys or contests. Participation in these is voluntary, and you may choose whether or not to participate. Information requested may include contact information (such as name and shipping address), and demographic information (such as zip code or age level). Contact information will be used to notify winners and award prizes, while survey information will be used to improve the site.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Other Disclosures</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Other Disclosures</h2>
                 <p>We may provide your personal information to third-party service providers who help us with various site services. These service providers do not have the right to share your information except pursuant to legal requirements such as a subpoena or warrant.</p>
                 <p>If the assets of the website are acquired by another company, that company will possess the personal information collected by us and will assume the rights and obligations as described in this Privacy Policy.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Other Limits to Your Privacy</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Other Limits to Your Privacy</h2>
                 <p>Our Site contains links to other third-party websites (“Linked Websites”). We are not responsible for the privacy practices or the content of Linked Websites. We encourage you to review the privacy statements of those sites.</p>
                 <p>Any information you disclose on chat groups, forums, and message boards becomes public information and is not subject to this Privacy Policy.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Updates and Changes to Privacy Policy</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Updates and Changes to Privacy Policy</h2>
                 <p>We reserve the right to update or change this Privacy Policy at any time by posting the changes on the Site. Any updates will be effective immediately upon posting.</p>
             </section>
 
             <section>
-                <h2 class="text-2xl font-semibold text-gray-800">Contact Information</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 mt-4">Contact Information</h2>
                 <p>If you have any questions about this Privacy Policy, you may contact us via email at <a href="mailto:support@fcanationalservice.org" class="text-blue-600">support@fcanationalservice.org</a>.</p>
             </section>
 
