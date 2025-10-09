d6b84c5161df27ab0d82bf20ad14f4602d46968f
Junatan  Marc Cadayona <ljohnolayvar32@gmail.com>
2025-10-09 21:57:59 +0800

    fix favicon

--- resources/views/rideshare/landing.blade.php
+++ resources/views/rideshare/landing.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FCA - Rideshare Sexual Abuse Claims</title>
-    <link rel="icon" type="image/x-icon" href="/static/favicon.ico">
+    <!-- Favicon  -->
+    <link rel="icon" href="{{ route('getFile', $_siteSettings['logo']) }}" />
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script>
